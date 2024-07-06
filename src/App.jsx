86587df45a488ab73871fd7dd43bd0b8a0b368aa
yurocarostyk <152785136+yurocarostyk@users.noreply.github.com>
2024-07-06 23:12:34 +0200

    taskl

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 // src/App.jsx
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
-import ContactForm from './components/ContactForm/ContactForm';
-import SearchBox from './components/SearchBox/SearchBox';
 import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactForm from './components/ContactForm/ContactForm';
 import styles from './App.module.css';
 
-const App = () => {
+function App() {
   const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    return savedContacts ? JSON.parse(savedContacts) : [];
+    const initialContacts = localStorage.getItem("contacts");
+    if (initialContacts !== null) {
+      return JSON.parse(initialContacts);
+    }
+    return [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ];
   });
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
+  const [filter, setFilter] = useState("");
   const addContact = ({ name, number }) => {
     const newContact = {
       id: nanoid(),
@@ -49,3 +52,5 @@ const App = () => {
 };
 
 export default App;
+
+
